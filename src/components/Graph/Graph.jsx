028b1f5b0492a81a668a7de8225f2924e42540bb
𝔻ℕ𝕠𝕣𝕕𝕚𝕔𝕍𝕫𝕝𝕒𝕟 <sthlm412@gmail.com>
2020-07-31 20:56:23 +0200

    add dailyData to the chart

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { line, Bar } from 'react-chatjs-2'; //wrapper for the charts  but need to install chart.js as well
+
+import stykes from './Graph.module.css';
 
 const Graph = () => {
     const [dailyData, setDailyData] = useState({});
 
     useEffect(() => {
+      const fetchAPIDaily = async () => {
+        setDailyData( await fetchDailyData());
+      }  
 
-    });
+      console.log(dailyData);
+
+      fetchAPIDaily();
+    }, []);
     
+    const lineChart = (
+        dailyData[0]
+        ? (
+        <line
+          data={{
+              labels: dailyData(({ date }) => date),
+              datasets: [{
+                  data: dailyData(({ confirmed}) => confirmed),
+                  label: 'Infections',
+                  borderColor: '#3333ff',
+                  fill: true,
+              }, {
+                  data: dailyData(({ deaths }) => deaths),
+                  label: 'Decease',
+                  borderColor: 'red',
+                  backgroundColor: 'rgba(255, 10, 10 )',
+                  fill: true,
+              }],
+          }}
+        />) : null
+    );
     return (
         <h1>Graph</h1>
     )
