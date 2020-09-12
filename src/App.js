00c7c8151210e50f93fceca918b9f4adb1355063
David The Developer <david.lopez@hyperisland.se>
2020-09-12 10:51:26 +0200

    Update App.js
    review api and removed image from footer

--- src/App.js
+++ src/App.js
@@ -34,6 +34,13 @@ class App extends React.Component {
         <CountrySelector handleCountryChange={this.handleCountryChange}/>
         <Graph data={ data } country={country} />  
      </div>
+      <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/TheDavidDev/"> The Dave Dev</a> 
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="TheDavidDev logo" />
+        </div>
         )
     }
 }
