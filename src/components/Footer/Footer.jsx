0add45468e8d118915941d907159254014c3372b
𝔻ℕ𝕠𝕣𝕕𝕚𝕔𝕍𝕫𝕝𝕒𝕟 <sthlm412@gmail.com>
2020-10-15 18:13:11 +0200

    add module css for the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from './Footer.module.css';
+
 
 function Footer() {
     return (
         <div>
-            Copyright 2020
+           Powered and Developed by{" "}
+        <a  href="https://www.TheDavid.dev/" target="_blank" rel="noopener noreferrer">
+          The David Dev
+        </a>{" "}
+        |  What is the{" "} <a  href="https://covid19.apple.com/screening" target="_blank" rel="noopener noreferrer">CORONA Virus ?</a>
+        
         </div>
     )
 }
